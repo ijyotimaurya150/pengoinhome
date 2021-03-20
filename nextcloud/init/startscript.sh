932e21b3f35abaaad422faa18cddee6f80757c41
Ubuntu <ubuntu@ip-172-31-36-94.ap-south-1.compute.internal>
2021-03-20 18:56:39 +0000

    Added nextcloud

---
+++ nextcloud/init/startscript.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+service apache2 restart
+cp /root/files/index.html /web/html/index.html
+cp /root/files/setup-nextcloud.php /web/html/setup-nextcloud.php
+chown -R www-data:www-data /web
+echo "Running an useless command to keep container in fg"
+tail -f /dev/null
